7da97246327bd38660443160a9ab1af58a605bb9
quruc90 <qruczbalu@gmail.com>
2024-12-22 16:58:55 +0100

    Updated connection string

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var cors = require("cors");
 const multer = require("multer");
 
 var app=Express();
-app.use(cors());
+app.unsubscribe(cors());
 
-var CONNECTION_STRING="mongodb+srv://qruczbalu:Fortress2@qurucbongo.rhkzh.mongodb.net/?retryWrites=true&w=majority&appName=QurucBongo";
+var CONNECTION_STRING="mongodb+srv://qruczbalu:mng90Qrc+RM4k@qurucbongo.axk6f.mongodb.net/?retryWrites=true&w=majority&appName=QurucBongo";
 
+var DATABASENAME="project"
+var db;
+
+app.listen(5038),()=>{
+
+}
