15ac13dd36a845821b5b440cfe954c220ec8cb22
quruc90 <qruczbalu@gmail.com>
2024-12-22 16:48:47 +0100

    Added connection string for the MongoDB database

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var Express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var cors = require("cors");
 const multer = require("multer");
+
+var app=Express();
+app.use(cors());
+
+var CONNECTION_STRING="mongodb+srv://qruczbalu:Fortress2@qurucbongo.rhkzh.mongodb.net/?retryWrites=true&w=majority&appName=QurucBongo";
+
