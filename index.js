07abcdcf794440e053fde10ea34dec0b46bbc10c
quruc90 <qruczbalu@gmail.com>
2024-12-22 17:14:01 +0100

    Connection is successful on port 4000, and database collection can be displayed

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ var cors = require("cors");
 const multer = require("multer");
 
 var app=Express();
-app.unsubscribe(cors());
+app.use(cors());
 
 var CONNECTION_STRING="mongodb+srv://qruczbalu:mng90Qrc+RM4k@qurucbongo.axk6f.mongodb.net/?retryWrites=true&w=majority&appName=QurucBongo";
 
-var DATABASENAME="project"
+var DATABASENAME="project";
 var db;
 
-app.listen(5038),()=>{
+app.listen(5038,()=>{
+    MongoClient.connect(CONNECTION_STRING,(error,client)=>{
+        db=client.db(DATABASENAME);
+        console.log("MongoDB Connected successfully");
+    })
+})
 
-}
+app.get('/api/cardealership/getcars',(request,response)=>{
+    db.collection("cars").find({}).toArray((error,result)=>{
+        response.send(result);
+    })
+})
